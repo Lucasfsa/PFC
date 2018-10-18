99a785d8b4c574b226a0b62bc5011da0644ae114
Matheus Rodrigues <matheushranjos@gmail.com>
2018-10-18 11:48:32 -0300

    Biblioteca de Ícones Adicionada

--- resources/views/corpo/homepage.blade.php
+++ resources/views/corpo/homepage.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 
 <html lang="pt-br">
-  
+
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
+
     <title>BC Organizer</title>
     <link rel="icon" href="{{ asset('imagem/icon.png') }}">
 
@@ -14,54 +14,54 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
 
   </head>
-  
-  <body>  
-        
+
+  <body>
+
         <nav class="navbar">
           <div class="container">
             <div class="navbar-header">
-         
+
               <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#barra-navegacao">
-              
+
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
 
               </button>
-              
-              
+
+
                 <img class="img-responsive grow" id="logo" src="{{ asset('imagem/img001.png') }}">
            </div>
 
             <div id="barra-navegacao" class="navbar-collapse collapse">
-              
+
               <ul class="nav navbar-nav navbar-right">
-                
-                <li> 
-                  <a href="http://www.dataautomacao.com.br">Data Automação</a> 
+
+                <li>
+                  <a href="http://www.dataautomacao.com.br">Data Automação</a>
                 </li>
-               
-              </ul> 
+
+              </ul>
 
             </div>
 
           </div>
         </nav>
 
-     
-      
+
+
         <div  align="center" id="corpo" >
         <img   class="img-responsive" id="img01" src="{{ asset('imagem/logo.png') }}">
-        </div>         
-     
+        </div>
+
       <div id="acess" align="center" >
       <a type="button" id="one" class="btn" href="{{ asset('login') }}">Entrar</a>
       </div>
-       
 
-  
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> 
-    <script src="{{ asset('bootstrap/js/bootstrap.min.js') }}"></script>    
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="{{ asset('bootstrap/js/bootstrap.min.js') }}"></script>
   </body>
 </html>
